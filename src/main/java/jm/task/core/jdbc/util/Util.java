8a7ab5b1b25e63e9812cd459adc28db7da8a6986
HotlineEugene <aleshin.evgeniy@outlook.com>
2024-11-08 18:11:33 +0300

    устанавливаем соединение

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,16 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static Connection getConnection() throws SQLException {
+        Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/mysql",
+                "root",
+                "4619622790"
+        );
+        return con;
+    }
 }
